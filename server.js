6e29c074502a9acbc0a59cdfa637e3e30b03d229
YanaLiz <lapa.270519@gmail.com>
2023-04-27 21:43:13 +0300

    add

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const app = require('./app')
-const mongoose = require("mongoose")
-// YH3ipxD7pi.WZYq
+
+
+
+mongoose.set("strictQuery", true)
+
+const DB_HOST = "mongodb+srv://Yana:YH3ipxD7pi.WZYq@cluster0.lo7i8zv.mongodb.net/db-contacts?retryWrites=true&w=majority"
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000)
+  })
+  .catch(error => {
+    console.log(error.message)
+    process.exit(1);
+  })
 
 
