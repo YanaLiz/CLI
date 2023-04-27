ee3da1f723669b98fe954d83048e24dea9bc6d13
YanaLiz <lapa.270519@gmail.com>
2023-04-27 21:33:27 +0300

    ADD

--- server.js
+++ server.js
@@ -4,12 +4,6 @@ const mongoose = require("mongoose")
 
 
 
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000")
-// })
-
-
-
 mongoose.set("strictQuery", true)
 
 const DB_HOST = "mongodb+srv://Yana:YH3ipxD7pi.WZYq@cluster0.lo7i8zv.mongodb.net/db-contacts?retryWrites=true&w=majority"
