cb137ab3bab328def82dd5bd1c8bab8ea6fbdd5d
YanaLiz <lapa.270519@gmail.com>
2023-05-21 22:36:54 +0300

    add

--- controllers/auth/resendVerify.js
+++ controllers/auth/resendVerify.js
@@ -1,14 +1,14 @@
 const { HttpError, sendEmail } = require('../../helpers');
 const { User } = require('../../models/user');
 require('dotenv').config()
-const { PATH } = process.env;
+const { PATH_WAY } = process.env;
 
 const resendVerify = async (res, req) => {
   const { email } = req.body
   const user = await User.find({ email });
   if (!user) throw HttpError(404, "User not found");
   if(user.verify) throw HttpError(400, "Verification has already been passed");
-  await sendEmail({ to: user.email, subject: "Hello ✔", html: `<a target="_blank" href="${PATH}/api/auth/verify/${user.verificationToken}">click here to verify</a>` });
+  await sendEmail({ to: user.email, subject: "Hello ✔",html: `<a target="_blank" href="${PATH_WAY}/api/auth/verify/${user.verificationToken}">click here to verify</a>` });
   };
 
 module.exports = resendVerify;
